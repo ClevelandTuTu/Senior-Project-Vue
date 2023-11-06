397f4c3d2f6171a3dd1425f3aa4132c409f63098
Jing Wang <jxw1335@case.edu>
2023-11-05 22:14:03 -0500

    calendar view is down

--- senior-project-frontend/src/components/DailyLogRecord.vue
+++ senior-project-frontend/src/components/DailyLogRecord.vue
@@ -1,6 +1,6 @@
 <template>
     <br>
-
+    <!--
     <table class="table table-responsive recordTable">
         <thead>
             <tr>
@@ -30,8 +30,7 @@
                 </td>
             </tr>
         </tbody>
-    </table>
-    
+    </table>-->
 
     <!--
     <div class="row">
@@ -59,6 +58,49 @@
     </div>
     -->
 
+    <div class="test">
+        <el-calendar v-model="selectedDay">
+        <template #header="{ date }">
+            <span style="font-weight: bold;">{{ date }}</span>
+            <el-button-group>
+                <el-button size="large" @click="selectDate('prev-month')" :icon="ArrowLeft">
+                <ArrowLeft />Previous Month
+                </el-button>
+                <el-button size="large" @click="selectDate('today')">Today</el-button>
+                <el-button size="large" @click="selectDate('next-month')">
+                Next Month
+                </el-button>
+            </el-button-group>
+        </template>
+        <template #date-cell="{ data }">
+            <div :class="data.isSelected ? 'is-selected' : ''">
+                {{ data.day.split('-').slice(1).join('-') }}
+                {{ data.isSelected ? '✔️' : '' }}
+                <div v-if="haveRecordOnDay(data.day)" style="text-align: center;">
+                    <div v-if="getMoodOnDay(data.day) == 0">
+                        <font-awesome-icon icon="fa-regular fa-face-sad-tear" style="font-size: 4em" />
+                    </div>
+                    <div v-else-if="getMoodOnDay(data.day) == 1">
+                        <font-awesome-icon icon="fa-regular fa-face-frown" style="font-size: 4em" />
+                    </div>
+                    <div v-else-if="getMoodOnDay(data.day) == 2">
+                        <font-awesome-icon icon="fa-regular fa-face-meh" style="font-size: 4em" />
+                    </div>
+                    <div v-else-if="getMoodOnDay(data.day) == 3">
+                        <font-awesome-icon icon="fa-regular fa-face-smile-beam" style="font-size: 4em" />
+                    </div>
+                    <div v-else-if="getMoodOnDay(data.day) == 4">
+                        <font-awesome-icon icon="fa-regular fa-face-laugh-beam" style="font-size: 4em" />
+                    </div>
+                </div>
+                <div v-else>
+                    
+                </div>
+            </div>
+        </template>
+    </el-calendar>
+    </div>
+
 </template>
 
 <script>
@@ -69,6 +111,7 @@ export default ({
     //components: {DailyLog},
     data() {
         return {
+            selectedDay: new Date(),
             records: [
                 
             ],
@@ -111,6 +154,53 @@ export default ({
             .catch(error => {
                 console.log(error)
             })
+        },
+        haveRecordOnDay(date) {
+            return this.records.some((record) => record.date === date)
+        },
+        getRecordOnDay(date) {
+            return this.records.find((record) => record.date === date)
+        },
+        getMoodOnDay(date) {
+            var record = this.getRecordOnDay(date);
+            if (record != null) {
+                return record.mood
+            }
+        },
+        selectDate(val) {
+            const currentMonth = this.selectedDay.getMonth();
+            const currentYear = this.selectedDay.getFullYear();
+
+            if (val === "today") {
+                this.selectedDay = new Date();
+            }
+            else if (val === "prev-month") {
+                const prevMonthDate = new Date(currentYear, currentMonth - 1, this.selectedDay.getDate());
+
+                // if last month has the same date
+                if (prevMonthDate.getMonth() === currentMonth - 1) {
+                    this.selectedDay = prevMonthDate;
+                } 
+                else {
+                    // if last month Does NOT has the same date, go to the Last Day of that month
+                    // special note: 0 of current month is last day of last month!!
+                    this.selectedDay = new Date(currentYear, currentMonth, 0); // 切换到上一个月的最后一天
+                }
+                //this.selectedDay = new Date(this.selectedDay.setMonth(this.selectedDay.getMonth() - 1))
+            }
+            else if (val === "next-month") {
+                const nextMonthDate = new Date(currentYear, currentMonth + 1, this.selectedDay.getDate());
+
+                // if last month has the same date
+                if (nextMonthDate.getMonth() === currentMonth + 1) {
+                    this.selectedDay = nextMonthDate;
+                } 
+                else {
+                    // if last month Does NOT has the same date, go to the Last Day of that month
+                    this.selectedDay = new Date(currentYear, currentMonth + 2, 0); // 切换到下一个月的最后一天
+                }
+                //this.selectedDay = new Date(this.selectedDay.setMonth(this.selectedDay.getMonth() + 1))
+            }
         }
     }
 })
@@ -129,4 +219,10 @@ export default ({
 .table {
     margin: auto;
 }
+.is-selected {
+  color: #1989fa;
+}
+.test /deep/  .el-calendar-table .el-calendar-day{
+    height: 8em;
+  }
 </style>
--- senior-project-frontend/src/main.js
+++ senior-project-frontend/src/main.js
@@ -4,8 +4,26 @@ import 'jquery/dist/jquery.min'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faFaceSadTear } from '@fortawesome/free-regular-svg-icons'
+import { faFaceFrown } from '@fortawesome/free-regular-svg-icons'
+import { faFaceMeh } from '@fortawesome/free-regular-svg-icons'
+import { faFaceSmileBeam } from '@fortawesome/free-regular-svg-icons'
+import { faFaceLaughBeam } from '@fortawesome/free-regular-svg-icons'
+/* add icons to the library */
+library.add(faFaceSadTear, faFaceFrown, faFaceMeh, faFaceSmileBeam, faFaceLaughBeam)
 
 const app = createApp(App);
-app.use(router).mount('#app')
+app.use(router)
+app.use(ElementPlus)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
 
 
